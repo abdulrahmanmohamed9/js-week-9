448dab7b5fe44bac6bdb21bbef0936d54b10e617
abdulrahmanmohamed9 <106539440+abdulrahmanmohamed9@users.noreply.github.com>
2023-12-28 02:12:13 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+var siteNameInput = document.getElementById("siteName");
+var siteUrlInput = document.getElementById("siteUrl");
+var bookMarks;
+if (localStorage.getItem("bookMark") != null) {
+  bookMarks = JSON.parse(localStorage.getItem("bookMark"));
+  displayTable();
+} else {
+  bookMarks = [];
+}
+function addBookMark() {
+  var bookMark = {
+    websiteName: siteNameInput.value,
+    visit: siteUrlInput.value,
+  };
+  bookMarks.push(bookMark);
+  displayTable();
+  clearInputs();
+}
+function clearInputs() {
+  siteNameInput.value = "";
+  siteUrlInput.value = "";
+}
+function displayTable() {
+  var trs = "";
+  for (var i = 0; i < bookMarks.length; i++) {
+    trs += `<tr>
+              <td>${i}</td>
+              <td>${siteNameInput.value}</td>
+              <td>
+                <a
+                  href="https://${siteUrlInput.value}"
+                  style="background-color: #9eb23b;"
+                  class=" btn btn-submit px-3 text-white"
+                  id="visitBtn"
+                >
+                  <i class="fa-solid fa-eye me-1"></i>
+                  Visit
+                </a>
+              </td>
+              <td>
+                <button
+                  onclick="deleteBookmark(${i})"
+                  type="button"
+                  class="btn btn-submit px-3 bg-danger text-white"
+                  id="deleteBtn"
+                >
+                  <i class="fa-solid fa-trash me-1"></i>
+                  Delete
+                </button>
+              </td>
+            </tr>`;
+  }
+  localStorage.setItem("bookMarks", JSON.stringify(bookMarks));
+
+  document.getElementById("tableContent").innerHTML = trs;
+}
+function deleteBookmark(index) {
+  bookMarks.splice(index, 1);
+  displayTable();
+}
